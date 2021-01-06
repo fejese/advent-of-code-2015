5cd28b1ca7e59b82599e91a6bc41da5e0cbed8d4
Endre Fejes <fejese@fejese.com>
2021-01-06 18:57:00 +0000

    12

---
+++ day-12/solv-2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import json
+
+# INPUT_FILE_NAME: str = "test-input-2"
+INPUT_FILE_NAME: str = "input"
+
+
+def sum_numbers(data) -> int:
+    if isinstance(data, int):
+        return data
+    if isinstance(data, list):
+        return sum(sum_numbers(e) for e in data)
+    if isinstance(data, dict):
+        if "red" in data.values():
+            return 0
+        return sum(sum_numbers(e) for e in data.values())
+    return 0
+
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    for line in input_file:
+        print(sum_numbers(json.loads(line)))

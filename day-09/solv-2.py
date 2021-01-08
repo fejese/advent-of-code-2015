01d1df38c59f847d82d198940b2ffab103a03e53
Endre Fejes <fejese@fejese.com>
2021-01-08 13:38:17 +0000

    9

---
+++ day-09/solv-2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import re
+from itertools import permutations as P
+from typing import Dict, List, Tuple, Set
+
+# INPUT_FILE_NAME: str = "test-input"
+INPUT_FILE_NAME: str = "input"
+
+LINE_PATTERN: re.Pattern = re.compile(r"^(\S+) to (\S+) = (\d+)$")
+
+distances: Dict[Tuple[str, str], int] = {}
+cities: Set[str] = set()
+with open(INPUT_FILE_NAME, "r") as input_file:
+    for line in input_file:
+        m = LINE_PATTERN.match(line)
+        if not m:
+            raise Exception(f"Can't parse line: {line}")
+
+        distances[tuple(sorted([m[1], m[2]]))] = int(m[3])
+        cities.add(m[1])
+        cities.add(m[2])
+
+
+print(
+    max(
+        sum(
+            distances[tuple(sorted([perm[i], perm[i + 1]]))]
+            for i in range(len(perm) - 1)
+        )
+        for perm in P(cities)
+    )
+)
